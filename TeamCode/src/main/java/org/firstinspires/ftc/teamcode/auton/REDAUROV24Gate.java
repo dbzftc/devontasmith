8eec61ded790f395c6ae6795e7fdf136cb10ab3e
shivm14 <shivmehtap14@gmail.com>
2026-03-12 01:09:31 -0400

    we mog PA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/REDAUROV24Gate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/REDAUROV24Gate.java
@@ -1,4 +1,4 @@
-﻿package org.firstinspires.ftc.teamcode.auton;
+package org.firstinspires.ftc.teamcode.auton;
 
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -25,8 +25,8 @@ import org.firstinspires.ftc.teamcode.extensions.DbzOpMode;
 import org.firstinspires.ftc.teamcode.opmodes.V2RED;
 
 @Config
-@Autonomous(name = "REDAUTOV2Gate")
-public class REDAUTOV24Gate extends DbzOpMode {
+@Autonomous(name = "REDAUROV2Gate")
+public class REDAUROV24Gate extends DbzOpMode {
 
     public static double servooffset = 0.01;
     public static double Push0 = 0.81;
@@ -39,7 +39,7 @@ public class REDAUTOV24Gate extends DbzOpMode {
     public static double holdClosePos = 0.8;
     public static double dthresh = 0.24;
     public static double detectionDebounce = 0.5;
-    public static double intakeWaitTimeout = 0.5;
+    public static double intakeWaitTimeout = 0.9;
 
     public static double TV = 0;
     public static double hoodServoPos = 0.5;
@@ -67,8 +67,8 @@ public class REDAUTOV24Gate extends DbzOpMode {
 
     public static double startX = 114.2417;
     public static double startY = 133.472;
-    public static double gatex = 146.1;
-    public static double gatey = 59.85;
+    public static double gatex = 147.1;
+    public static double gatey = 60.2;
     public static double gateh = 24;
 
     public static double shootX = 88.149;
@@ -88,7 +88,7 @@ public class REDAUTOV24Gate extends DbzOpMode {
     public static double threshold = 220;
     public static double threshold2 = 180;
 
-    public static double nearWallX = 124.573;
+    public static double nearWallX = 127.573;
     public static double nearWallY = 84.566;
 
     public static double lowWallX = 127.207;
@@ -110,11 +110,11 @@ public class REDAUTOV24Gate extends DbzOpMode {
                     .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(0)).build();
 
             Path2 = follower.pathBuilder().addPath(
-                            new BezierCurve(new Pose(89.149, 84.168), new Pose(99.885, 54.734), new Pose(130.544, 59.850)))
+                            new BezierCurve(new Pose(89.149, 84.168), new Pose(99.885, 54.734), new Pose(127.544, 59.850)))
                     .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0)).build();
 
             Path3 = follower.pathBuilder().addPath(
-                            new BezierCurve(new Pose(130.544, 59.850), new Pose(99.704, 54.734), new Pose(89.149, 84.168)))
+                            new BezierCurve(new Pose(127.544, 59.850), new Pose(99.704, 54.734), new Pose(89.149, 84.168)))
                     .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0)).build();
 
             Path4 = follower.pathBuilder().addPath(
@@ -313,7 +313,7 @@ public class REDAUTOV24Gate extends DbzOpMode {
 
             case intakeWait1:
                 runBallDetection();
-                if (ballState == BallState.locked || stateTimer.seconds() >= 0.2) {
+                if (ballState == BallState.locked || stateTimer.seconds() >= 0.4) {
                     ballState = BallState.idle;
                     wasDetected = false;
                     follower.followPath(paths.Path5, true);
@@ -693,7 +693,7 @@ public class REDAUTOV24Gate extends DbzOpMode {
         if (autonState == AutonState.shoot1 || autonState == AutonState.followPath1) {
             rawAngle = 40;
         } else if (autonState == AutonState.shoot13 || autonState == AutonState.followPath13) {
-            rawAngle = 50;
+            rawAngle = 80;
         } else {
             rawAngle = 44;
         }
